8b8cd6f9aaa7bad8f1b5060e320d40f9a6ccbac3
이호찬 <1ghcks@gmail.com>
2019-06-10 21:28:34 +0900

    first commit

---
+++ thing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  thing
+//
+//  Created by 이호찬 on 10/06/2019.
+//  Copyright © 2019 mashup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
