c106002286560d1d9ccbc564b27b6c3a29e17dbd
Jinha <j_2j@naver.com>
2019-06-18 00:08:43 +0900

    푸시 테스트

---
+++ thing/Model/UserModel.swift
@@ -0,0 +1,16 @@
+//
+//  UserModel.swift
+//  thing
+//
+//  Created by 박진하 on 17/06/2019.
+//  Copyright © 2019 mashup. All rights reserved.
+//
+
+import Foundation
+
+struct UserModel: Decodable {
+    let dateBirth: Double
+    let gender: Int
+    let nickName: String
+    let uid: String
+}
