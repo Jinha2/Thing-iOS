3d9a94e33516a9bf9ad9d4951829ba120942103f
Jinha <j_2j@naver.com>
2019-06-29 22:54:33 +0900

    Search 구현중

---
+++ thing/Search/SearchWordTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SearchWordTableViewCell.swift
+//  thing
+//
+//  Created by Jinha Park on 2019/06/29.
+//  Copyright © 2019 mashup. All rights reserved.
+//
+
+import UIKit
+
+class SearchWordTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
