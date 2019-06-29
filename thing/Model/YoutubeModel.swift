9e12a12841084bc08c5307532a27eb0de577be34
Jinha <j_2j@naver.com>
2019-06-30 04:08:19 +0900

    카테고리 작업중

---
+++ thing/Model/YoutubeModel.swift
@@ -0,0 +1,18 @@
+//
+//  YoutubeModel.swift
+//  thing
+//
+//  Created by Jinha Park on 2019/06/30.
+//  Copyright © 2019 mashup. All rights reserved.
+//
+
+import Foundation
+
+struct YoutubeModel: Decodable {
+    let thumbnailUrl: String
+    let profileUrl: String
+    let title: String
+    let rank: String
+    let subscriberCount: String
+    let viewCount: String
+}
