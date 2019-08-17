883af24f23f3c313ebcc6ac1d9da3504ddbcacdf
Jinha2 <j_2j@naver.com>
2019-08-17 23:12:49 +0900

    태그 추가 뷰 구현중 (테이블뷰로 바꿀 예정쓰)

--- thing/Main/AddTagViewController.swift
+++ thing/Main/AddTagViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import Tags
 
 final class AddTagViewController: UIViewController {
+    @IBOutlet weak var commontagsView: TagsView!
+    @IBOutlet weak var categoryTagsView: TagsView!
+
+    private let tags = [Tags]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        commontagsView.delegate = self
+        categoryTagsView.delegate = self
 
         requestTags()
     }
@@ -24,9 +32,55 @@ final class AddTagViewController: UIViewController {
 extension AddTagViewController {
     private func requestTags() {
         ThingProvider.tags(completion: { tags in
-            Log(tags)
+            defer {
+                self.commontagsView.redraw()
+                self.categoryTagsView.redraw()
+            }
+
+            let normalOptions = ButtonOptions(
+                layerRadius: 20,
+                layerWidth: 0,
+                tagTitleColor: UIColor(named: "background")!,
+                tagFont: UIFont.systemFont(ofSize: 16, weight: .medium),
+                tagBackgroundColor: UIColor(named: "boxBackground")!
+            )
+
+            let selectedOptions = ButtonOptions(
+                layerColor: UIColor(named: "MainRed")!,
+                layerRadius: 20,
+                layerWidth: 1.5,
+                tagTitleColor: UIColor(named: "MainRed")!,
+                tagFont: UIFont.systemFont(ofSize: 16, weight: .medium)
+            )
+
+            for tag in tags.tags {
+
+                if tag.category == "공통" {
+                    for title in tag.list {
+                        let tagButton = TagButton()
+
+                        tagButton.setTitle(title, for: .normal)
+                        tagButton.setEntity(selectedOptions)
+
+                        self.commontagsView.append(tagButton)
+                    }
+                } else {
+                    let tagButton = TagButton()
+
+                    tagButton.setTitle(tag.category, for: .normal)
+                    tagButton.setEntity(normalOptions)
+
+                    self.categoryTagsView.append(tagButton)
+                }
+            }
         }) { error in
             Log(error)
         }
+    }
+}
+
+extension AddTagViewController: TagsDelegate {
+    func tagsTouchAction(_ tagsView: TagsView, tagButton: TagButton) {
+        print(tagButton)
     }
 }
